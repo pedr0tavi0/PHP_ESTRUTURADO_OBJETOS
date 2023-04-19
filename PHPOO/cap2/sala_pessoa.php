7fc56569dcbfebff1642f1557504300de44173d6
pedr0tavi0 <112214113+pedr0tavi0@users.noreply.github.com>
2023-04-18 22:12:28 -0300

    arquivo de conexão

---
+++ PHPOO/cap2/sala_pessoa.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'classes/pessoa.php';
+require_once 'classes/sala_virtual.php';
+
+
+
+$s1 = new SalaVirtual('Diagramas uml','Engenharia de Software',20);
+
+
+
+
+
+
+
+
+
+
+
+
+?>
